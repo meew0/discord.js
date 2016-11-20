4df13983377ce8fbd555fb9264d5b7b69be7f298
meew0 <blactbt@live.de>
2016-11-20 17:30:05 +0100

    Update deploy.sh to also build the webpack

--- docs/deploy/deploy.sh
+++ docs/deploy/deploy.sh
@@ -4,7 +4,11 @@
 set -e
 
 function build {
+  # Build docs
   node docs/generator/generator.js
+
+  # Build the webpack
+  VERSIONED=false npm run web-dist
 }
 
 # Ignore Travis checking PRs
@@ -31,32 +35,16 @@ if [ -n "$TRAVIS_TAG" ]; then
   SOURCE=$TRAVIS_TAG
 fi
 
+# Initialise some useful variables
 REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-TARGET_BRANCH="docs"
-
-# Checkout the repo in the target branch so we can build docs and push to it
-git clone $REPO out -b $TARGET_BRANCH
-cd out
-cd ..
-
-# Build the docs
-build
-
-# Move the generated JSON file to the newly-checked-out repo, to be committed
-# and pushed
-mv docs/docs.json out/$SOURCE.json
-
-# Commit and push
-cd out
+# Configure git
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-git add .
-git commit -m "Docs build: ${SHA}"
-
+# Decrypt and add the ssh key
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
@@ -66,5 +54,37 @@ chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
 
-# Now that we're all set up, we can push.
+# Build everything
+build
+
+# Checkout the repo in the target branch so we can build docs and push to it
+TARGET_BRANCH="docs"
+git clone $REPO out -b $TARGET_BRANCH
+
+# Move the generated JSON file to the newly-checked-out repo, to be committed
+# and pushed
+mv docs/docs.json out/$SOURCE.json
+
+# Commit and push
+cd out
+git add .
+git commit -m "Docs build: ${SHA}"
+git push $SSH_REPO $TARGET_BRANCH
+
+# Clean up...
+cd ..
+rm -rf out
+
+# ...then do the same once more for the webpack
+TARGET_BRANCH="webpack"
+git clone $REPO out -b $TARGET_BRANCH
+
+# Move the generated webpack over
+mv webpack/discord.js out/discord-$SOURCE.js
+mv webpack/discord-min.js out/discord-$SOURCE-min.js
+
+# Commit and push
+cd out
+git add .
+git commit -m "Webpack build: ${SHA}"
 git push $SSH_REPO $TARGET_BRANCH
